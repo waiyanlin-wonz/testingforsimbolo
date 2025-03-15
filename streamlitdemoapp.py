02dfc655d841f3c164e046933164c385ea138e30
Wai Yan Lin <121044827+waiyanlin-wonz@users.noreply.github.com>
2025-03-15 16:19:24 +0630

    Update streamlitdemoapp.py

--- streamlitdemoapp.py
+++ streamlitdemoapp.py
@@ -6,7 +6,7 @@ st.title("Welcome to My Streamlit Web Page")
 
 st.subheader("This is a simple Streamlit application")
 
-st.write("Streamlit makes it easy to build interactive web apps with Python.")
+st.write("Testing for assignment 4")
 
 user_input = st.text_input("Enter your name:")
 st.write(f"Hello, {user_input}!")
@@ -14,7 +14,7 @@ st.write(f"Hello, {user_input}!")
 st.button("Click Me")
 
 if st.checkbox("Show more info"):
-    st.write("Streamlit is an open-source app framework for Machine Learning and Data Science projects.")
+    st.write("assignment 4")
 
 st.sidebar.header("Sidebar Menu")
 st.sidebar.write("You can add more options here.")
